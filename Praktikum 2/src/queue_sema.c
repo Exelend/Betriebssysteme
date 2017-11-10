1ab3537e9108de577cf68f29565c01007a65273f
Hauke Goldhammer <hauke.goldhammer@haw-hamburg.de>
2017-11-10 12:50:26 +0100

    Kommentare in die Queue Dateien eingefuegt

--- Praktikum 2/src/queue_sema.c
+++ Praktikum 2/src/queue_sema.c
@@ -22,6 +22,9 @@ static struct Queue* queue = NULL;
 static sem_t semaphorFull;
 static sem_t semaphorEmpty;
 
+/*
+ * Initialisier eine Queue mit dazu gehoerigen Semaphoren.
+ */
 int queue_sema_init(){
     int returnValue = sem_init(&semaphorEmpty, PRIVATE, BUFFERSIZE);
     if(returnValue < 0){
@@ -35,11 +38,17 @@ int queue_sema_init(){
     return EXIT_SUCCESS;
 }
 
+/*
+ * Erstellt fur den uebergebenen Char ein Node und fuegt diesen in die Queue ein.
+ */
 int setLoad_sema(char load){
+    //Thread blockiert, wenn die Queue leer ist
     sem_wait(&semaphorEmpty);
+    //Node wird erstellt und gefuellt
     NODE* node = (NODE*) malloc(sizeof(NODE));
     node->load = load;
     node->next = NULL;
+    //Kritischer Abschnitt Anfang
     pthread_mutex_lock(&mutex);
 
     if(queue->head == NULL){
@@ -50,30 +59,43 @@ int setLoad_sema(char load){
         queue->tail = node;
     }
 
+    //Kritischer Abschnitt Ende
     pthread_mutex_unlock(&mutex);
+    //Teilt dem Consumer mit, dass ein neues Element vorhanden ist
     sem_post(&semaphorFull);
     return EXIT_SUCCESS;
 }
 
+/*
+ * Erstellt fur den uebergebenen Char ein Node und fuegt diesen in die Queue ein.
+ */
 char getLoad_sema(){
+    //Ueberpruefung ob ueberhaupt ein Element in der Queue, wenn nicht blockiert der Thread
     sem_wait(&semaphorFull);
+    //Kritischer Abschnitt Anfang
     pthread_mutex_lock(&mutex);
-
+    //Ueberpruefung ob der Thread sterben soll
     if(consumerKill){
         return '\n';
     }
-    
+
     NODE* node = queue->head;
     queue->head = node->next;
 
+    //Kritischer Abschnitt Ende
     pthread_mutex_unlock(&mutex);
+    //Den Produzern mitteilen, dass wieder Platz in der Queue ist
     sem_post(&semaphorEmpty);
+    //Den Char auslesen und den Node freibebn
     char temp = node->load;
     free(node);
     node = NULL;
     return temp;
 }
 
+/*
+ * Zerstoert die Semaphoren und die Queue.
+ */
 void destroy_sema_queue(){
     pthread_mutex_unlock(&mutex);
     sem_destroy(&semaphorFull);
